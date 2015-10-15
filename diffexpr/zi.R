46007ae5e81357fea2eab7b74bf3568c07eeb706
kieranrcampbell <kieranrcampbell@gmail.com>
2015-10-15 14:37:53 +0100

    added zero inflation comparison script (zi.R)

---
+++ diffexpr/zi.R
@@ -0,0 +1,64 @@
+
+# Testing utility of zero-inflation ---------------------------------------
+library(embeddr)
+
+
+base_dir <- ""
+system <- devtools::session_info()$platform$system
+if(length(grep("darwin", system)) > 0) {
+  # we're on the mac
+  base_dir <- "~/mount/"
+} else {
+  # we're on linux
+  base_dir <- "/net/isi-scratch/kieran/"
+}
+
+
+devtools::load_all(paste0(base_dir, "switch/sctools"))
+source(paste0(base_dir, "GP/pseudogp2/diffexpr/prep_data.R"))
+
+get_map <- function(x) mlv(x,method = "HSM")$M
+
+data <- load_data(base_dir)
+sce <- data$sce ; pst <- data$pst
+
+pst_map <- apply(pst, 2, get_map)
+sce$pseudotime <- pst_map
+
+gene <- match("TUBBP1", fData(sce)$gene_short_name)
+
+plot_in_pseudotime(sce[gene,])
+x <- exprs(sce)[gene,]
+
+## non-zero inflated model
+model <- norm_fit_alt_model(x, pseudotime(sce))
+
+zimodel <- alt_EM_ctrl(x, pseudotime(sce), loglik_tol = 1e-8)
+
+plt1 <- norm_plot_model(model, x, pseudotime(sce))
+plt2 <- norm_zi_plot_model(zimodel$par, x, pseudotime(sce))
+
+cowplot::plot_grid(plt1, plt2, ncol = 2)
+
+# Plot the differences ----------------------------------------------------
+
+imputed <- zimodel$x
+df <- data.frame(x = x, t = pseudotime(sce), imp = imputed)
+dfm <- reshape2::melt(df, id.vars = "t")
+
+mu_func <- function(t, params) {
+  L <- params[1] ; k <- params[2] ; t_0 <- params[3] ; r <- params[4]
+  L / (1 + exp(-k*(t - t_0)))
+}
+
+blue <- ggthemes::ggthemes_data$fivethirtyeight['blue']
+red <- ggthemes::ggthemes_data$fivethirtyeight['red']
+ggplot(dfm) + geom_point(aes(x = t, y = value, colour = variable), size =2.5) +
+  ggthemes::scale_colour_fivethirtyeight() +
+  stat_function(fun = mu_func, args = list(model$par), color=blue) +
+  stat_function(fun = mu_func, args = list(zimodel$par), color = red) +
+  
+ggsave(filename = paste0(base_dir, "GP/pseudogp2/diffexpr/zi.png"),
+       width = 3, height = 2, scale = 3)
+
+
