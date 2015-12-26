40bdfc5b36d6cc3a6ab6bf888427acead846d0a1
Kieran Campbell <kieranrcampbell@gmail.com>
2015-12-26 20:43:27 +0000

    finished monocle comparison, figures generated

--- analysis/figs/mstcomp/mstcomp.R
+++ analysis/figs/mstcomp/mstcomp.R
@@ -4,56 +4,76 @@ library(rhdf5)
 library(readr)
 library(coda)
 library(MCMCglmm)
+library(cowplot)
+#library(pseudogp)
+library(GGally)
+
+# the pseudogp version does matrix standardizing
+standardize <- function(t) (t - min(t))/(max(t) - min(t))
+
 
 base_dir <- "~/mount/"
-setwd(file.path(base_dir, "GP/pseudogp2/stan/gbio/mstcomp"))
-source(file.path(base_dir, "GP/pseudogp2/stan/diffexpr/monocle/prep_data.R"))
-sce <- load_data(base_dir)
+trace_file <- file.path(base_dir, "pseudogp-paper/data/monocle_stan_traces.h5")
+sce_file <- file.path(base_dir, "pseudogp-paper/data/sce_monocle.Rdata")
+embeddings_file <- file.path(base_dir, "pseudogp-paper/data/trapnell_embeddings.h5")
+fig_path <- file.path(base_dir, "pseudogp-paper/analysis/figs/mstcomp/")
 
-h5file <- file.path(base_dir, "GP/pseudogp2/data/stan_traces_for_gbio.h5")
-X <- h5read(h5file, "X")
+X <- h5read(embeddings_file, "Xle")
 
-sce <- plotPCA(sce, return_SCESet = TRUE)
-plotReducedDim(sce)
+load(sce_file)
+sce <- sce_23
 
-# cds <- toCellDataSet(sce)
-# sce2 <- fromCellDataSet(cds, use_exprs_as = "exprs")
-# plotReducedDim(sce2)
-
+n_cells_exprs <- rowSums(exprs(sce) > sce@lowerDetectionLimit)
+genes_to_use <- n_cells_exprs > (0.1 * ncol(sce)) # select genes expressed in at least 10% of cells
+sce <- sce[genes_to_use,]
 
 # comparison of pseudotimes -----------------------------------------------
 
 redDim(sce) <- X
-plotReducedDim(sce)
+#plotReducedDim(sce)
 cds <- toCellDataSet(sce)
 cds <- orderCells(cds)
-cds_le_pseudotime <- cds$Pseudotime
+cds_le_pseudotime <- standardize(cds$Pseudotime)
 
-# df <- read_csv(file.path("~/mount", "GP/pseudogp2/R_notebooks/df_fit.csv"))
-# to_use <- df$gene_names[df$for_embedding]
-# cds <- setOrderingFilter(cds, to_use)
-# cds <- reduceDimension(cds, use_irlba = FALSE)
-# cds <- orderCells(cds)
-# cds_ica_pseudotime <- cds$Pseudotime
-# plot_spanning_tree(cds)
+rv <- matrixStats::rowVars(exprs(sce))
+ntop <- 500
+feature_set <- 
+  order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
+cds <- setOrderingFilter(cds, featureNames(sce)[feature_set])
+cds <- reduceDimension(cds, use_irlba = FALSE)
+cds <- orderCells(cds)
+#plot_spanning_tree(cds, color_by = "pseudotime")
+monocle_pst <- cds$Pseudotime
 
-tmcmc <- mcmc(h5read(h5file, "pst"))
+tmcmc <- mcmc(h5read(trace_file, "pst"))
 tmap <- posterior.mode(tmcmc)
 intervals <- HPDinterval(tmcmc, prob = 0.95)
-dft <- data.frame(monocle = cds_le_pseudotime, tmap = tmap, intervals)
+
+dft <- data.frame(monocle = monocle_pst, tmap = tmap, intervals, monocle_le = cds_le_pseudotime)
 
 R2 <- signif(cor(dft$monocle, dft$tmap)^2, digits = 2)
 R2text <- as.expression(substitute(italic(R)^2 == r, list(r = R2)))
 
-cplt <- ggplot(dft) + geom_point(aes(x = monocle, y = tmap)) + 
-  geom_errorbar(aes(x = monocle, ymin = lower, ymax = upper), alpha = 0.6, width = 0.3) +
+cplt <- ggplot(dft) + geom_point(aes(x = monocle_pst, y = tmap)) + 
+  geom_errorbar(aes(x = monocle_pst, ymin = lower, ymax = upper), alpha = 0.6, width = 1) +
   xlab("Monocle estimate") + ylab("GPLVM MAP estimate") +
-  geom_text(x = 3, y = 0.9, label = as.character(R2text), parse = TRUE)
+  geom_text(x = 2, y = 0.9, label = as.character(R2text), parse = TRUE)
+
+R2le <- signif(cor(dft$monocle_le, dft$tmap)^2, digits = 2)
+R2textle <- as.expression(substitute(italic(R)^2 == r, list(r = R2le)))
+
+leplt <- ggplot(dft) + geom_point(aes(x = monocle_le, y = tmap)) + 
+  geom_errorbar(aes(x = monocle_le, ymin = lower, ymax = upper), alpha = 0.6, width = 0.05) +
+  xlab("Monocle estimate") + ylab("GPLVM MAP estimate") +
+  geom_text(x = 0.1, y = 0.9, label = as.character(R2textle), parse = TRUE)
+
+corplts <- plot_grid(cplt, leplt, labels = c("ICA", "LE"), nrow = 1)
+
+ggsave(file.path(fig_path, "S1_monocle_comparison.png"), corplts, width = 7, height = 3)
 
 
 set.seed(123)
 nsamples <- 30
-standardize <- function(t) (t - min(t))/(max(t) - min(t))
 ncells <- ncol(sce)
 pseudosamples <- vector("list", ncells)
 gt <- orderCells(cds)$Pseudotime
@@ -61,7 +81,7 @@ gt <- standardize(gt)
 
 # did you ever see such awful R in your life
 for(i in 1:nsamples) {
-  chosen <- sample(ncells, round(0.7*ncells), replace=FALSE)
+  chosen <- sample(ncells, round(0.8*ncells), replace=FALSE)
   cds_copy <- cds[,chosen]
   cds_copy@reducedDimS <- cds@reducedDimS[,chosen]
   cds_copy <- orderCells(cds_copy)
@@ -78,11 +98,9 @@ pst_means <- sapply(pseudosamples, mean)
 pst_sd <- sapply(pseudosamples, sd)
 pst_95 <- 2*pst_sd
 
-plot(gt, pst_means)
 
 dplt <- ggplot(data.frame(pst_95)) + geom_density(aes(x = pst_95), fill = "darkred") +
   xlab(expression(2 * sigma)) + ylab("Density")
 
-pg <- cowplot::plot_grid(cplt, dplt, labels = c("A", "B"), nrow = 1)
-cowplot::ggsave(pg, file = "S2_monocle_comparison.png", width = 8, height = 3, scale = 1.2)
+cowplot::ggsave(dplt, file = file.path(fig_path, "S2_monocle_comparison.png"), width = 4, height = 3, scale = 1.2)
 
