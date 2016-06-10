2d35c3f04bd4bb50c3c973ae35a0a1de81a763aa
Kieran Campbell <kieranrcampbell@gmail.com>
2016-06-10 17:21:57 +0100

    false discovery rate -> false discovery ratio

--- analysis/diffexpr/3_fdr_calculation.R
+++ analysis/diffexpr/3_fdr_calculation.R
@@ -2,12 +2,11 @@
 # Differential expression comparison for uncertainty in pseudotime
 # kieran.campbell@sjc.ox.ac.uk
 
-library(scater)
-library(embeddr)
+
 library(readr)
-library(MCMCglmm)
-library(coda)
-library(rhdf5)
+library(magrittr)
+library(ggplot2)
+library(dplyr)
 
 alpha <- 0.05
 
@@ -27,8 +26,40 @@ prop_sig <- ap %>% group_by(gene) %>%
 
 sig_df <- inner_join(mapp, prop_sig, by = "gene")
 
-ggplot(sig_df, aes(x = q_val, y = prop_sig)) + geom_point()
+designate <- function(qval, prop_sig) {
+  if(qval < alpha & prop_sig >= (1 - alpha)) {
+    return("True positive")
+  } else if(qval < alpha & prop_sig < (1 - alpha)) {
+    return("False positive")
+  } else if(qval > alpha & prop_sig < (1 - alpha)) {
+    return("True negative") 
+  } else {
+    return("False negative")
+  }
+}
 
-sig_df %<>% mutate(false_disc = q_val < alpha & prop_sig < (1 - alpha))
+discovery_types <- sig_df %>%
+  select(q_val, prop_sig) %>%
+  apply(1, function(row) designate(row[1], row[2]))
 
-print(paste("False discovery rate for study", study, "is", mean(sig_df$false_disc)))
+sig_df %<>% mutate(discovery_type = discovery_types)
+
+num_fp <- sum(sig_df$discovery_type == "False positive")
+num_tp <- sum(sig_df$discovery_type == "True positive")
+
+fpr <- num_fp / (num_fp + num_tp)
+
+print(paste("False discovery ratio for study", study, "is", fpr))
+
+sig_df %<>% mutate(percent_sig = 100 * prop_sig)
+
+ggplot(sig_df, aes(x = percent_sig, y = q_val, fill = discovery_type)) +
+  geom_point(alpha = 0.5, shape = 21, color = 'black', size = 2.3) +
+  cowplot::theme_cowplot() +
+  scale_fill_brewer(name = element_blank(), palette = "Set1") +
+  xlab("% MCMC traces significant") +
+  ylab("FDR-adjusted MAP p-value") +
+  theme(legend.position = c(0.7,0.8)) +
+  geom_hline(yintercept = alpha, colour = "grey", linetype = 2) +
+  geom_vline(xintercept = 100 * (1 - alpha), colour = "grey", linetype = 2) +
+  ggtitle(paste("FDR analysis for", study, "et al."))
