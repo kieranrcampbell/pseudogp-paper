52b8e9a8f2dcd1566c9ba855680a84cc66f4cbcf
Kieran Campbell <kieranrcampbell@gmail.com>
2016-08-19 16:05:52 +0100

    finished rev

--- analysis/figs/boostrap/4_analysis.R
+++ analysis/figs/boostrap/4_analysis.R
@@ -6,6 +6,7 @@ library(MCMCglmm)
 library(coda)
 library(cowplot)
 library(goseq)
+library(magrittr)
 
 alpha <- 0.05
 
@@ -14,11 +15,15 @@ gplvm_dir <- "data/bootstrap/gplvm_de"
 
 bootstrap_files <- dir(bootstrap_dir)
 bootstrap_files <- bootstrap_files[grep("de", bootstrap_files)]
+bootstrap_files <- bootstrap_files[grep("0", bootstrap_files)]
+
 
 bootstrap_dfs <- lapply(bootstrap_files, function(bs) {
-  bootstrap_no <- as.integer(gsub("de_", "", gsub(".csv", "", bs, fixed = TRUE)))
+  parsed <- strsplit(bs, "_")[[1]]
+  prop_sampled <- as.numeric(parsed[2])
+  boostrap_no <- gsub(".csv", "", parsed[3], fixed = TRUE)
   dx <- read_csv(file.path(bootstrap_dir, bs))
-  dx <- mutate(dx, Bootstrap = bootstrap_no)
+  dx <- mutate(dx, Bootstrap = bootstrap_no, prop_sampled)
   dx
 })
 
@@ -38,7 +43,7 @@ gplvm_dfs <- lapply(gplvm_files, function(bs) {
 gplvm_df <- bind_rows(gplvm_dfs)
 rm(gplvm_dfs)
 
-bootstrap_psig <- group_by(bootstrap_df, gene) %>% 
+bootstrap_psig <- group_by(bootstrap_df, gene, prop_sampled) %>% 
   summarise(psig = mean(q_val < alpha), mean_qval = mean(q_val))
 
 gplvm_psig <- group_by(gplvm_df, gene) %>% 
@@ -46,32 +51,35 @@ gplvm_psig <- group_by(gplvm_df, gene) %>%
 
 df <- inner_join(bootstrap_psig, gplvm_psig, by = "gene", suffix = c("_bootstrap", "_gplvm"))
 
+df$prop_sampled <- plyr::mapvalues(df$prop_sampled, from = c(0.5, 0.7, 0.9),
+                                   to = c("50%", "70%", "90%"))
 
 pltA <- ggplot(df, aes(x = psig_bootstrap, y = psig_gplvm)) + geom_point(alpha = 0.4) +
   xlab("Proportion significant bootstrap") +
-  ylab("Proportion significant \nGPLVM") # + theme_classic()
+  ylab("Proportion significant \nGPLVM") +
+  facet_wrap(~ prop_sampled)
 
-# ggsave("~/Desktop/reviewer1.png")
+ggsave("~/Desktop/reviewer1.png", width = 10, height = 4)
 
 pltB <- ggplot(df, aes(x = mean_qval_bootstrap, y = mean_qval_gplvm)) + geom_point(alpha = 0.4) +
   xlab("Mean Q-val bootstrap") +
-  ylab("Mean Q-val \nGPLVM") + # theme_classic() +
+  ylab("Mean Q-val \nGPLVM") + facet_wrap(~ prop_sampled) +
   stat_function(fun = function(x) x, color = 'red')
 
-# ggsave("~/Desktop/reviewer2.png")
+ggsave("~/Desktop/reviewer2.png", width = 10, height = 4)
 
 
 # More plots
 
-disp_df <- filter(df, psig_gplvm < 0.75) %>% 
+disp_df <- filter(df, prop_sampled == 0.9, psig_gplvm < 0.75) %>% 
   mutate(disparity = abs(psig_bootstrap - psig_gplvm)) %>% 
   arrange(desc(disparity))
 
-agree_df <- filter(df, psig_bootstrap > 0.95, psig_gplvm > 0.95) %>% 
+agree_df <- filter(df, prop_sampled == 0.9, psig_bootstrap > 0.95, psig_gplvm > 0.95) %>% 
   mutate(disparity = abs(psig_bootstrap - psig_gplvm)) %>% 
   arrange(disparity)
 
-bad_ensembl_id <- disp_df$gene[1]
+bad_ensembl_id <- disp_df$gene[2]
 myog_ensembl_id <- featureNames(sce)[grep("MYOG", fData(sce)$gene_short_name)]
 
 filter(agree_df, gene == myog_ensembl_id)
@@ -88,7 +96,7 @@ gex_good <- exprs(sce)[myog_ensembl_id, ]
 
 bad_hgnc_symbol <- fData(sce)[bad_ensembl_id, ]$gene_short_name
 
-lower_plt <- data_frame(VDAC3 = gex_bad, MYOG = gex_good, pst_map) %>% 
+lower_plt <- data_frame(PSPHP1 = gex_bad, MYOG = gex_good, pst_map) %>% 
   gather(Gene, Expression, -pst_map) %>% 
   ggplot(aes(x = pst_map, y = Expression)) + geom_point() +
   cowplot::theme_cowplot() + stat_smooth(se = FALSE, color = "red") +
@@ -96,12 +104,12 @@ lower_plt <- data_frame(VDAC3 = gex_bad, MYOG = gex_good, pst_map) %>%
   facet_wrap(~ Gene)
 
 
-upper_grid <- plot_grid(pltA, pltB, nrow = 1, labels = "AUTO")
-lower_grid <- plot_grid(lower_plt, labels = "C", scale = 0.85)
+# upper_grid <- plot_grid(pltA, pltB, nrow = 1, labels = "AUTO")
+lower_grid <- plot_grid(lower_plt, scale = 0.8)
 
-plot_grid(upper_grid, lower_grid, nrow = 2)
+plot_grid(pltA, pltB, lower_grid, labels = "AUTO", nrow = 3)
 
-ggsave("figs/bootstrap/bootstrap_fig.png", width = 10, height = 8)
+ggsave("figs/bootstrap/bootstrap_fig.png", width = 9, height = 8)
 
 
 # Compare bootstrapped pseudotimes to gplvm pseudotimes -------------------
@@ -121,5 +129,15 @@ data_frame(gex_gp_bad, pst_map) %>%
 
 # GO time -----------------------------------------------------------------
 
+# UpsetR plot time --------------------------------------------------------
 
+library(UpSetR)
 
+df9 <- filter(df, prop_sampled == 0.9)
+
+ulist = list(bootstrap = filter(df9, psig_bootstrap > 0.95) %>% extract2("gene"),
+             gplvm = filter(df9, psig_gplvm > 0.95) %>% extract2("gene"))
+
+png("~/Desktop/upset.png")
+upset(fromList(ulist))
+dev.off()
